9f4dcd541decbb2cf8c9158378c0ad6e819bf9b1
Amisha-Sinha <83289237+Amisha-Sinha@users.noreply.github.com>
2024-04-10 17:25:48 +0530

    Add files via upload

---
+++ assets/js/essay1.js
@@ -0,0 +1,41 @@
+let currentPage = 1;
+const totalPages = 3;
+
+
+function showPage(pageNumber) {
+  const pages = document.querySelectorAll('.story-page');
+  pages.forEach(page => page.style.display = 'none');
+  document.getElementById(`page${pageNumber}`).style.display = 'block';
+  updateButtonVisibility();
+}
+
+function prevPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    showPage(currentPage);
+  }
+}
+
+function nextPage() {
+  if (currentPage < totalPages) {
+    currentPage++;
+    showPage(currentPage);
+  }
+}
+
+function updateButtonVisibility() {
+  if (currentPage === 1) {
+    prevBtn.style.display = 'none';
+  } else {
+    prevBtn.style.display = 'inline-block';
+  }
+
+  if (currentPage === totalPages) {
+    nextBtn.style.display = 'none';
+  } else {
+    nextBtn.style.display = 'inline-block';
+  }
+}
+
+// Show the first page initially
+showPage(currentPage);
